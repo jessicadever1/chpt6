91aea2541738f9c7f9c513a7689fd2b6d5844f52
Jess Dever <jessicadever1@gmail.com>
2021-03-01 10:27:32 -0600

    initial commit

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,22 @@
 // Put your code here
+
+let namesArray = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"
+]
+
+console.log("These are the names in the namesArray")
+
+namesArray.forEach(name => console.log(name))
+
+console.log(" ")
+
+let theNamesArray = namesArray.filter(name => name.startsWith("The"))
+console.log("'The' Place Names")
+theNamesArray.forEach(name => console.log(name))
